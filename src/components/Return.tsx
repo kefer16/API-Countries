74c675eec37826ba6fd69fac49805690f03e67ed
kefer <morafi.1999.16@gmail.com>
2024-01-23 23:54:07 -0500

    se cambia funcionalidad al hacer back

--- src/components/Return.tsx
+++ src/components/Return.tsx
@@ -1,14 +1,18 @@
 import "../styles/Return.scss";
 import KeyboardBackspaceRoundedIcon from "@mui/icons-material/KeyboardBackspaceRounded";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Return = () => {
+   const navigate = useNavigate();
+   const goBack = () => {
+      navigate(-1);
+   };
    return (
       <div className="background-body container">
          <div className="return-container container-max">
-            <Link to="/" className="return-container-button">
+            <div onClick={goBack} className="return-container-button">
                <KeyboardBackspaceRoundedIcon /> Back
-            </Link>
+            </div>
          </div>
       </div>
    );
